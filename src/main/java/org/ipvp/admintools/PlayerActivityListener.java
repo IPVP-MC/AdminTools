08343cdd545265ad0455cfa2b7690a39bd852b1e
Matthew Steglinski <matthew.steglinski@mail.utoronto.ca>
2018-01-16 13:07:59 -0500

    Don't block netty threads when checking ban information on login

--- src/main/java/org/ipvp/admintools/PlayerActivityListener.java
+++ src/main/java/org/ipvp/admintools/PlayerActivityListener.java
@@ -6,6 +6,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
@@ -18,7 +19,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.UUID;
@@ -33,112 +36,94 @@ public class PlayerActivityListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler(priority = EventPriority.LOW)
-    public void onPreLogin(LoginEvent event) {
-        event.registerIntent(plugin);
-        UUID who = event.getConnection().getUniqueId();
-        try (Connection connection = plugin.getDatabase().getConnection()) {
-            Ban ban = plugin.getActiveBan(connection, who);
-            if (ban != null) {
-                event.setCancelled(true);
-                String expiry = ban.getExpiry() == null ? "Permanent" : TimeFormatUtil.toDetailedDate(ban.getExpiry().getTime());
-                event.setCancelReason(ChatColor.translateAlternateColorCodes('&',
-                        String.format("&cYou are banned from &lIPVP" +
-                                "\n\n&eReason: &f%s" +
-                                "\nExpires: &f%s" +
-                                "\n\n&eAppeal at http://ipvp.org/forum", ban.getReason(), expiry)));
-            } else {
-                String ip = event.getConnection().getAddress().getAddress().getHostAddress();
-                IpBan ipBan = plugin.getActiveIpBan(connection, ip);
-                if (ipBan != null) {
-                    event.setCancelled(true);
-                    event.setCancelReason("You are currently banned from the network!");
-                }
-            }
-        } catch (Exception e) {
-            event.setCancelled(true);
-            event.setCancelReason("An error occurred when checking your ban information");
-            plugin.getLogger().log(Level.SEVERE, "Failed to check user ban inforamtion", e);
-        } finally {
-            event.completeIntent(plugin);
-        }
-    }
-
     @EventHandler
     public void onLogin(LoginEvent event) {
         if (event.isCancelled()) {
             return;
         }
 
+        UUID who = event.getConnection().getUniqueId();
         event.registerIntent(plugin);
-        try (Connection connection = plugin.getDatabase().getConnection();
-             PreparedStatement insertLogin = connection.prepareStatement("INSERT INTO player_login(id, name, ip_address) " +
-                     "VALUES (?, ?, INET_ATON(?))")) {
-            Mute mute = plugin.getActiveMute(connection, event.getConnection().getUniqueId());
-            if (mute != null) {
-                plugin.registerMute(mute.getPunished(), mute);
-            }
+        plugin.getProxy().getScheduler().runAsync(plugin, () -> {
+            try (Connection connection = plugin.getDatabase().getConnection()) {
+                Ban ban = plugin.getActiveBan(connection, who);
+                if (ban != null) {
+                    event.setCancelled(true);
+                    String expiry = ban.getExpiry() == null ? "Permanent" : TimeFormatUtil.toDetailedDate(ban.getExpiry().getTime());
+                    event.setCancelReason(ChatColor.translateAlternateColorCodes('&',
+                            String.format("&cYou are banned from &lIPVP" +
+                                    "\n\n&eReason: &f%s" +
+                                    "\nExpires: &f%s" +
+                                    "\n\n&eAppeal at http://ipvp.org/forum", ban.getReason(), expiry)));
+                } else {
+                    List<String> alts = getBannedAlternateAccounts(connection, who);
 
-            PendingConnection pendingConnection = event.getConnection();
+                    // If the
+                    if (!alts.isEmpty()) {
+                        if (alts.size() > 5) {
+                            String formatted = alts.stream().limit(5).collect(Collectors.joining("\n&c"));
+                            event.setCancelled(true);
+                            event.setCancelReason(ChatColor.translateAlternateColorCodes('&',
+                                    String.format("&cYou are banned on too many accounts!" +
+                                            "\n&c%s" +
+                                            "\n&c..." +
+                                            "\n&eAppeal at http://ipvp.org/forum", formatted)));
+                            return;
+                        }
+
+                        // Broadcast that the player has some banned alternate accounts
+                        plugin.broadcast(ChatColor.RED + String.format("%s is an alternate account of the following banned players: %s",
+                                event.getConnection().getName(), alts.stream().collect(Collectors.joining(", "))), "admintools.notify.alts");
+                    }
+
+                    // Track the login information
+                    trackLogin(connection, event.getConnection());
+
+                    // Cache the active mute the player has
+                    Mute mute = plugin.getActiveMute(connection, event.getConnection().getUniqueId());
+                    if (mute != null) {
+                        plugin.registerMute(mute.getPunished(), mute);
+                    }
+                }
+            } catch (SQLException e) {
+                event.setCancelled(true);
+                event.setCancelReason("An error occurred when checking your ban information");
+                plugin.getLogger().log(Level.SEVERE, "Failed to log in user", e);
+            } finally {
+                event.completeIntent(plugin); // Allow the login event to proceed
+            }
+        });
+    }
+
+    private List<String> getBannedAlternateAccounts(Connection connection, UUID who) throws SQLException {
+        try (PreparedStatement findBannedAlts = connection.prepareStatement("SELECT DISTINCT name " +
+                "FROM player_login " +
+                "INNER JOIN (SELECT DISTINCT banned_id " +
+                "FROM player_active_punishment " +
+                "WHERE type = 'ban') AS b " +
+                "WHERE player_login.id = b.banned_id " +
+                "AND ip_address " +
+                "IN (SELECT DISTINCT ip_address " +
+                "FROM player_login " +
+                "WHERE id = ?)")) {
+            findBannedAlts.setString(1, who.toString());
+            try (ResultSet alts = findBannedAlts.executeQuery()) {
+                List<String> bannedAlts = new ArrayList<>();
+                while (alts.next()) {
+                    bannedAlts.add(alts.getString("name"));
+                }
+                return bannedAlts;
+            }
+        }
+    }
+
+    private void trackLogin(Connection connection, PendingConnection pendingConnection) throws SQLException {
+        try (PreparedStatement insertLogin = connection.prepareStatement("INSERT INTO player_login(id, name, ip_address) " +
+                     "VALUES (?, ?, INET_ATON(?))")) {
             insertLogin.setString(1, pendingConnection.getUniqueId().toString());
             insertLogin.setString(2, pendingConnection.getName());
             insertLogin.setString(3, pendingConnection.getAddress().getAddress().getHostAddress());
             insertLogin.executeUpdate();
-        } catch (SQLException e) {
-            plugin.getLogger().log(Level.SEVERE, "Failed to log player login", e);
-        } finally {
-            event.completeIntent(plugin);
-        }
-    }
-
-    @EventHandler
-    public void onLoginNotifyAlts(LoginEvent event) {
-        if (event.isCancelled()) {
-            return;
-        }
-
-        event.registerIntent(plugin);
-        try (Connection connection = plugin.getDatabase().getConnection();
-             PreparedStatement findBannedAlts = connection.prepareStatement("SELECT DISTINCT name " +
-                             "FROM player_login " +
-                             "INNER JOIN (SELECT DISTINCT banned_id " +
-                                "FROM player_active_punishment " +
-                                "WHERE type = 'ban') AS b " +
-                             "WHERE player_login.id = b.banned_id " +
-                             "AND ip_address " +
-                             "IN (SELECT DISTINCT ip_address " +
-                                "FROM player_login " +
-                                "WHERE id = ?)")) {
-            findBannedAlts.setString(1, event.getConnection().getUniqueId().toString());
-            try (ResultSet alts = findBannedAlts.executeQuery()) {
-                Set<String> bannedAlts = new TreeSet<>();
-                while (alts.next()) {
-                    bannedAlts.add(alts.getString("name"));
-                }
-
-                if (bannedAlts.isEmpty()) {
-                    return;
-                }
-
-                String formatted = bannedAlts.stream().limit(5).collect(Collectors.joining("\n&c"));
-
-                if (bannedAlts.size() > 5) {
-                    event.setCancelled(true);
-                    event.setCancelReason(ChatColor.translateAlternateColorCodes('&',
-                            String.format("&cYou are banned on too many accounts!" +
-                                    "\n&c%s" +
-                                    "\n&c..." +
-                                    "\n&eAppeal at http://ipvp.org/forum", formatted)));
-                    return;
-                }
-
-                plugin.broadcast(ChatColor.RED + String.format("%s is an alternate account of the following banned players: %s",
-                        event.getConnection().getName(), bannedAlts.stream().collect(Collectors.joining(", "))), "admintools.notify.alts");
-            }
-        } catch (SQLException e) {
-            plugin.getLogger().log(Level.SEVERE, "Failed to notify player alt login", e);
-        } finally {
-            event.completeIntent(plugin);
         }
     }
 
@@ -155,7 +140,6 @@ public class PlayerActivityListener implements Listener {
         if (mute == null) {
             return;
         }
-        // TODO: Better notification messages
 
         if (event.isCommand()) {
             if (isMutedCommand(event.getMessage())) {
